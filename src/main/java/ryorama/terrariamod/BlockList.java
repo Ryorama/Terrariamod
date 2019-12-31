a3703d9854889a71aad62d7e7a1f6cb7220ee98d
Ryan <redryma@icloud.com>
2019-12-31 07:57:15 -0800

    First upload

---
+++ src/main/java/ryorama/terrariamod/BlockList.java
@@ -0,0 +1,13 @@
+package ryorama.terrariamod;
+
+import net.minecraft.block.Block;
+
+public class BlockList
+{
+	public static Block copper_ore;
+	public static Block grass;
+	public static Block dirt;
+	public static Block log;
+	public static Block leaves;
+	public static Block stone;
+}
