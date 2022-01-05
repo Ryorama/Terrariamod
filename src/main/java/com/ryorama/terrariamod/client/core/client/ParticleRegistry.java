28c5ca39e21176d595ebc104039fcc4f029723e4
Ryan <redryma@icloud.com>
2022-01-05 14:01:51 -0500

    We got particles!

--- src/main/java/com/ryorama/terrariamod/client/core/client/ParticleRegistry.java
+++ src/main/java/com/ryorama/terrariamod/client/core/client/ParticleRegistry.java
@@ -1,8 +1,14 @@
 package com.ryorama.terrariamod.client.core.client;
 
 import com.ryorama.terrariamod.TerrariaMod;
+import com.ryorama.terrariamod.client.fx.particles.LeafParticle;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.screen.PlayerScreenHandler;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -12,5 +18,14 @@ public class ParticleRegistry {
 
     public static void init() {
         Registry.register(Registry.PARTICLE_TYPE, new Identifier(TerrariaMod.MODID, "leaf"), FOREST_LEAF);
+    }
+
+    @Environment(EnvType.CLIENT)
+    public static void initClient() {
+        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture, registry) -> {
+            registry.register(new Identifier(TerrariaMod.MODID, "particle/leaf"));
+        }));
+
+        ParticleFactoryRegistry.getInstance().register(ParticleRegistry.FOREST_LEAF, LeafParticle.Factory::new);
     }
 }
