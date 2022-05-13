56719bf0d265d4a9f047aa4542f7fb1b937c4fb9
Ryan <redryma@icloud.com>
2022-05-13 09:09:58 -0400

    crap

--- src/main/java/com/ryorama/terrariamod/entity/IBoss.java
+++ src/main/java/com/ryorama/terrariamod/entity/IBoss.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.util.Identifier;
 
-@Environment(EnvType.CLIENT)
 public abstract interface IBoss {
 
 	public abstract Identifier bossIcon();
